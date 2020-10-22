41aa05117d92a40e1477a2f91f170a4a814f95a8
DjAlok123 <69834328+DjAlok123@users.noreply.github.com>
2020-10-22 15:49:17 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,43 @@
+var movingRect, fixedRect;
+
+function setup() {
+  createCanvas(1200, 800);
+  // createSprite(400, 200, 50, 50);
+  movingRect = createSprite(400, 200, 80, 30);
+  fixedRect = createSprite(200, 200, 50, 80);
+  movingRect.shapeColor = "green";
+  fixedRect.shapeColor = "green";
+}
+
+function draw() {
+  background(255);
+  
+  movingRect.x = World.mouseX;
+  movingRect.y = World.mouseY;
+  
+  // if (movingRect.isTouching(fixedRect)) {
+  //   movingRect.shapeColor = "red";
+  //   fixedRect.shapeColor = "red";
+  // } else {
+  //   movingRect.shapeColor = "green";
+  //   fixedRect.shapeColor = "green";
+  // }
+
+  
+  isTouching();
+  drawSprites();
+}
+
+function isTouching() {
+  if (movingRect.x - fixedRect.x < movingRect.width/2 + fixedRect.width/2 && 
+    fixedRect.x - movingRect.x < movingRect.width/2 + fixedRect.width/2 && 
+    movingRect.y - fixedRect.y < movingRect.width/2 + fixedRect.width/2 &&
+    fixedRect.y - movingRect.y < movingRect.width/2 + fixedRect.width/2) {
+
+    movingRect.shapeColor = "red";
+    fixedRect.shapeColor = "red";
+  } else {
+    movingRect.shapeColor = "green";
+    fixedRect.shapeColor = "green";
+  }
+}
